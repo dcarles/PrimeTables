0b267b0a6689a721cc6e1572294d19424f698443
danielcarles <danielcarles@gmail.com>
2016-03-24 13:25:05 +0000

    Initial solution

---
+++ PrimeTables.Test/PrimeTableTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PrimeTables.Test
+{
+    [TestFixture]
+    public class PrimeTableTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
