c50e5637d3e8d9f9eacfbe937da19eaa026f0366
Daniel Carles <danielcarles@gmail.com>
2016-03-28 23:58:50 +0100

    Minor refactor, moved primeGenerator class to the Model project

---
+++ PrimeTables.Model/PrimeGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeTables.Model
+{
+    public class PrimeGenerator
+    {
+        public List<long> GeneratePrimeNumbers(long n)
+        {
+            if (n < 1)
+                throw new ArgumentException("N parameter must be greater than Zero");
+
+            return GetNPrimeNumbers(n);
+        }
+
+        private static List<long> GetNPrimeNumbers(long n)
+        {
+            //Create list with first prime in it
+            var primes = new List<long> { 2 };
+
+            var candidate = 3;
+            while (primes.Count < n)
+            {
+
+                if (IsPrime(candidate))
+                {
+                    primes.Add(candidate);
+                }
+
+                candidate += 2;
+            }
+
+            return primes;
+        }
+
+
+        private static bool IsPrime(int number)
+        {
+            //We get the limiting int by square rooting number 
+            var max = Convert.ToInt32(Math.Sqrt(number));
+
+            if (number == 1 || number % 2 == 0)
+                return false;
+
+            if (number == 2)
+                return true;
+
+            for (int i = 2; i <= max; i++)
+            {
+                if ((number % i) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
--- PrimeTables.Test/PrimeNumbersTests.cs
+++ PrimeTables.Test/PrimeNumbersTests.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using PrimeTables.Model;
 
 namespace PrimeTables.Test
 {
@@ -95,59 +96,5 @@ namespace PrimeTables.Test
 
     }
 
-    public class PrimeGenerator
-    {
-        public List<long> GeneratePrimeNumbers(long n)
-        {
-            if (n < 1)
-                throw new ArgumentException("N parameter must be greater than Zero");
-
-            return GetNPrimeNumbers(n);
-        }
-
-        private static List<long> GetNPrimeNumbers(long n)
-        {
-            //Create list with first prime in it
-            var primes = new List<long> {2};
-           
-            var candidate = 3;
-            while (primes.Count < n)
-            {
-
-                if (IsPrime(candidate))
-                {
-                    primes.Add(candidate);
-                }
-
-                candidate += 2;
-            }
-
-            return primes;
-        }
-
-
-        private static bool IsPrime(int number)
-        {
-            //We get the limiting int by square rooting number 
-            var max = Convert.ToInt32(Math.Sqrt(number));
-
-            if (number == 1 || number % 2 == 0)
-                return false;
-
-            if (number == 2)
-                return true;
-
-            for (int i = 2; i <= max; i++)
-            {
-                if ((number % i) == 0)
-                    return false;
-            }
-
-            return true;
-        }
-
-
-
-
-    }
+ 
 }
