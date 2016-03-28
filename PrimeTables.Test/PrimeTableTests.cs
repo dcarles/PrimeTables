f06deb660d0a1c0e815276b35733834152965917
Daniel Carles <danielcarles@gmail.com>
2016-03-28 23:23:46 +0100

    simple tests showcasing TDD style

--- PrimeTables.Test/PrimeTableTests.cs
+++ PrimeTables.Test/PrimeTableTests.cs
@@ -10,17 +10,44 @@ namespace PrimeTables.Test
     public class PrimeNumbersTests
     {
         [Test]
-        public void GetNPrimeNumbers_Should_Return_List_With_2_If_N_Is_One()
+        public void GetNPrimeNumbers_Should_Throw_ArgumentException_If_N_Is_Less_Than_One()
         {
             //Arrange
-            var expected = new List<int> { 2 };
+            //Act
+            TestDelegate primeNumbersDelegate = () => PrimeGenerator.GeneratePrimeNumbers(0);
+            //Assert
+            Assert.That(primeNumbersDelegate, Throws.ArgumentException);
+
+        }
+
+        [Test]
+        public void GetNPrimeNumbers_Should_Return_List_With_Two_If_N_Is_One()
+        {
+            //Arrange
+            var expected = new List<long> { 2 };
             var n = 1;
 
             //Act
-            var actual = PrimeGenerator.GetNPrimeNumbers(n).ToList();
+            var actual = PrimeGenerator.GeneratePrimeNumbers(n).ToList();
 
             //Assert
-            Assert.AreEqual(n,actual.Count());
+            Assert.AreEqual(n, actual.Count());
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void GetNPrimeNumbers_Should_Return_List_With_Two_And_Three_If_N_Is_Two()
+        {
+            //Arrange
+            var expected = new List<long> { 2, 3 };
+            var n = 2;
+
+            //Act
+            var actual = PrimeGenerator.GeneratePrimeNumbers(n).ToList();
+
+            //Assert
+            Assert.AreEqual(n, actual.Count());
             CollectionAssert.AreEqual(expected, actual);
 
         }
@@ -29,9 +56,28 @@ namespace PrimeTables.Test
 
     public class PrimeGenerator
     {
-        public static IEnumerable<int> GetNPrimeNumbers(int i)
+        public static IEnumerable<long> GeneratePrimeNumbers(long n)
         {
-            yield return 2;
+            if(n <1)
+            throw new ArgumentException("N parameter must be greater than Zero");
+
+           return GetNPrimeNumbers(n);
         }
+
+        private static IEnumerable<long> GetNPrimeNumbers(long n)
+        {
+
+            yield return 2;
+
+            for (var i = 3; i <= n + 1; i++)
+            {
+                yield return i;
+            }
+         
+        }
+
+
+        
+
     }
 }
