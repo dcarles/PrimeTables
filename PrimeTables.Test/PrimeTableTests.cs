68a07ba59750b3bbee2ffca3a7965f000759c843
Daniel Carles <danielcarles@gmail.com>
2016-03-28 22:25:50 +0100

    First initial test

--- PrimeTables.Test/PrimeTableTests.cs
+++ PrimeTables.Test/PrimeTableTests.cs
@@ -1,15 +1,37 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace PrimeTables.Test
 {
     [TestFixture]
-    public class PrimeTableTests
+    public class PrimeNumbersTests
     {
         [Test]
-        public void TestMethod1()
+        public void GetNPrimeNumbers_Should_Return_List_With_2_If_N_Is_One()
         {
-            Assert.AreEqual(1, 1);
+            //Arrange
+            var expected = new List<int> { 2 };
+            var n = 1;
+
+            //Act
+            var actual = PrimeGenerator.GetNPrimeNumbers(n).ToList();
+
+            //Assert
+            Assert.AreEqual(n,actual.Count());
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+    }
+
+    public class PrimeGenerator
+    {
+        public static IEnumerable<int> GetNPrimeNumbers(int i)
+        {
+            yield return 2;
         }
     }
 }
